146a7799b1916869410a9c5bada1d8f630889e5e
聂晓明 <1298295230@qq.com>
2025-03-20 08:51:36 +0800

    feat: update python file

--- 简单服务器/index.py
+++ 简单服务器/index.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+import requests
 
 app = Flask(__name__)
 # url_for('static', filename='index.html')
@@ -24,6 +26,25 @@ def form():
 def greet():
     name = request.form['name']
     return f"Hello, {name}! Welcome to my Flask app."
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return {
+        "name": "xiaoming"
+    }
+
+
+@app.route('/picture', methods=['POST'])
+def picture():
+    # url = "https://www.baidu.com/index.htm"
+    # res = requests.get(url, headers={
+    #     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.97 Safari/537.36 Core/1.116.484.400 QQBrowser/13.6.6320.400"
+    # })
+    # print(res)
+    return {
+        "name": "xiaoming"
+    }
 
 
 if __name__ == '__main__':
